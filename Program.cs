c51276e698b36056f65a3307341950cb8aaecb7e
lucasmardby <lucasmardby@gmail.com>
2023-12-09 09:13:29 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Globalization;
+using System.IO.Enumeration;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
+
+namespace First_Repository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student1 = new Student("Sophia", 93, 87, 98, 95, 100);
+            var student2 = new Student("Nicolas", 80, 83, 82, 88, 85);
+            var student3 = new Student("Zahirah", 84, 96, 73, 85, 79);
+            var student4 = new Student("Jeong", 90, 92, 98, 100, 97);
+            var student5 = new Student("Lucas", 100, 60, 60, 60, 60);
+
+            Console.WriteLine("Student\t\tGrade\n");
+            Console.WriteLine($"{student1.Name}{student1.FirstSpacing()}{student1.Score}\t{student1.Grading()}");
+            Console.WriteLine($"{student2.Name}{student2.FirstSpacing()}{student2.Score}\t{student2.Grading()}");
+            Console.WriteLine($"{student3.Name}{student3.FirstSpacing()}{student3.Score}\t{student3.Grading()}");
+            Console.WriteLine($"{student4.Name}{student4.FirstSpacing()}{student4.Score}\t{student4.Grading()}");
+            Console.WriteLine($"{student5.Name}{student5.FirstSpacing()}{student5.Score}\t{student5.Grading()}");
+
+        }
+
+    }
+
+}
---
+++ Student.cs
@@ -0,0 +1,96 @@
+﻿namespace First_Repository
+{
+    public class Student
+    {
+        public string Name;
+        private string Space;
+        private int Sum;
+        public decimal Score;
+        public string Grade;
+        //Class average method?
+        
+        /*
+        Add dictionary for subjects
+        Dictionary<string, int> openWith =
+            new Dictionary<string, int>();
+        */
+
+        private int Math;
+        private int Biology;
+        private int Physics;
+        private int Chemistry;
+        private int Programming;
+
+        public Student(string name, int math, int biology, int physics, int chemistry, int programming)
+        {
+            this.Name = name;
+            
+            this.Math = math;
+            this.Biology = biology;
+            this.Physics = physics;
+            this.Chemistry = chemistry;
+            this.Programming = programming;
+
+            Sum = math + biology + physics + chemistry + programming;
+            Score = Sum / 5.0m;
+        }
+        public string Grading()
+        {
+            switch (Score)
+            {
+                case > 97:
+                    Grade = "A+";
+                    return Grade;
+                case > 93:
+                    Grade = "A";
+                    return Grade;
+                case > 90:
+                    Grade = "A-";
+                    return Grade;
+                case > 87:
+                    Grade = "B+";
+                    return Grade;
+                case > 83:
+                    Grade = "B";
+                    return Grade;
+                case > 80:
+                    Grade = "B-";
+                    return Grade;
+                case > 77:
+                    Grade = "C+";
+                    return Grade;
+                case > 73:
+                    Grade = "C";
+                    return Grade;
+                case > 70:
+                    Grade = "C-";
+                    return Grade;
+                case > 67:
+                    Grade = "D+";
+                    return Grade;
+                case > 63:
+                    Grade = "D";
+                    return Grade;
+                case > 60:
+                    Grade = "D-";
+                    return Grade;
+                default:
+                    Grade = "E";
+                    return Grade;
+            }
+        }
+        public string FirstSpacing()
+        {
+            if (Name.Length > 6)
+            {
+                this.Space = ":\t";
+                return Space;
+            }
+            else
+            {
+                this.Space = ":\t\t";
+                return Space;
+            }
+        }
+    }
+}
